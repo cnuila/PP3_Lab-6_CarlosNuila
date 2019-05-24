bda6b0ae33ea3b49fbc912e37d9af35c73655a04
Carlos Nuila <cnuia14@icloud.com>
2019-05-24 14:25:36 -0600

    Iniciando Repositorio

---
+++ Carro.cpp
@@ -0,0 +1,58 @@
+#include "Carro.h"
+#include "Chasis.h"
+#include "Motor.h"
+#include "Pintura.h"
+#include <sstream>
+using namespace std;
+
+Carro::Carro(){
+    modelo = "";
+    numModelo = 0;
+}
+
+Carro::Carro(string mod,string numMod,Chasis cha,Motor mot,Pintura pint){
+    modelo = mod;
+    numModelo = numModelo;
+    chasis = cha;
+    motor = mot;
+    pintura = pint;
+}
+
+string Carro::getModelo(){
+    return modelo;
+}
+void Carro::setModelo(string mod){
+    modelo = mod;
+}
+
+int Carro::getNumModelo(){
+    return numModelo;
+}
+void Carro::setNumModelo(int numMod){
+    numModelo = numMod;
+}
+
+Chasis Carro::getChasis(){
+    return chasis;
+}
+void Carro::setChasis(Chasis cha){
+    chasis = cha;
+}
+
+Motor Carro::getMotor(){
+    return motor;
+}
+void Carro::setMotor(Motor mot){
+    motor = mot;
+}
+
+Pintura Carro::getPintura(){
+    return pintura;
+}
+void Carro::setPintura(Pintura pint){
+    pintura = pint;
+}
+
+string Carro::toString(){
+    return "Modelo: "+modelo+str(numModelo);
+}
---
+++ Carro.h
@@ -0,0 +1,37 @@
+#ifndef CARRO_H
+#define CARRO_H
+#include <string>
+using std::string;
+
+class Carro{
+
+    private:
+        string modelo;
+        int numModelo;
+        Chasis chasis;
+        Motor motor;
+        Pintura pintura;
+
+    public:
+        Carro();
+        Carro(string,string,Chasis,Motor,Pintura);
+
+        string getModelo();
+        void setModelo(string);
+
+        int getNumModelo();
+        void setNumModelo(int);
+
+        Chasis getChasis();
+        void setChasis(Chasis);
+
+        Motor getMotor();
+        void setMotor(Motor);
+
+        Pintura getPintura();
+        void setPintura(Pintura);
+
+        string toString();
+};
+
+#endif
